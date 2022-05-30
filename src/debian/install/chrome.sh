93b5c650558360eed9dfb566f2522051acea0066
Luiz Felipe Stangarlin <luizfelipestang@outlook.com>
2022-05-30 17:09:45 -0500

    fixed dbus launch and environment

--- src/debian/install/chrome.sh
+++ src/debian/install/chrome.sh
@@ -6,6 +6,3 @@ echo "Install Chromium Browser"
 apt-get update 
 apt-get install -y chromium chromium-l10n
 apt-get clean -y
-ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
-### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
-echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
--- src/debian/install/libnss_wrapper.sh
+++ src/debian/install/libnss_wrapper.sh
@@ -6,7 +6,6 @@ echo "Install nss-wrapper to be able to execute image as non-root user"
 apt-get update 
 apt-get install -y libnss-wrapper gettext
 apt-get clean -y
-#ln -s /usr/lib/x86_64-linux-gnu/libnss3.so /usr/lib/libnss_wrapper.so
 
 echo "add 'source generate_container_user.sh' to .bashrc"
 
