b73740d8341f8eb3817c37968b95408dcdbfea39
Luiz Felipe Stangarlin <luizfelipestang@outlook.com>
2022-05-31 16:19:03 -0500

    epoptes client config

--- src/common/scripts/epoptes_client_startup.sh
+++ src/common/scripts/epoptes_client_startup.sh
@@ -5,6 +5,13 @@ set -e
 # should also source $STARTUPDIR/generate_container_user.sh
 source $HOME/.bashrc
 
+if [[ $1 =~ -s|--skip ]]; then
+    echo -e "\n\n------------------ SKIP STARTUP -----------------"
+    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
+    echo "Executing command: '${@:2}'"
+    exec "${@:2}"
+fi
+
 ## correct forwarding of shutdown signal
 cleanup () {
     kill -s SIGTERM $!
@@ -13,11 +20,14 @@ cleanup () {
 trap cleanup SIGINT SIGTERM
 
 ## import the certificate
+echo -e "\n---------------- install certificate -----------------------"
+echo -e "export SERVER=$SERVER\n" > /etc/default/epoptes-client
 epoptes-client -c
 
-## start the service
-echo -e "\n---------------- start epoptes client ----------------------"
-/sbin/start-stop-daemon --start --oknodo --quiet -b --exec /usr/sbin/epoptes-client
+## configure session
+echo -e "\n------------------- config session -------------------------"
+useradd --uid 1000 --user-group --groups epoptes --home-dir $HOME/ --shell /bin/bash default_headless
+echo -e "[Desktop Entry]\nHidden=False" > $HOME/.config/autostart/epoptes-client.desktop
 
 ## cascade the next start script
 echo -e "\n------------------- vnc_startup.sh -------------------------"
