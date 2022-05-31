6125a26d45d79f150a017631d80e612bf04d2e10
Luiz Felipe Stangarlin <luizfelipestang@outlook.com>
2022-05-31 16:18:39 -0500

    epoptes server startup

--- src/common/scripts/epoptes_server_startup.sh
+++ src/common/scripts/epoptes_server_startup.sh
@@ -5,6 +5,13 @@ set -e
 # should also source $STARTUPDIR/generate_container_user.sh
 source $HOME/.bashrc
 
+if [[ $1 =~ -s|--skip ]]; then
+    echo -e "\n\n------------------ SKIP STARTUP -----------------"
+    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
+    echo "Executing command: '${@:2}'"
+    exec "${@:2}"
+fi
+
 ## correct forwarding of shutdown signal
 cleanup () {
     kill -s SIGTERM $!
@@ -14,8 +21,18 @@ trap cleanup SIGINT SIGTERM
 
 ## start the service
 echo -e "\n---------------- start epoptes server ----------------------"
-/sbin/start-stop-daemon --start --oknodo --quiet -b --exec /usr/sbin/epoptes
+if [[ $DEBUG == true ]]; then echo "/usr/bin/twistd3 epoptes"; fi
+/usr/bin/twistd3 epoptes &> $STARTUPDIR/epoptes_startup.log
+
+## configure session
+echo -e "\n------------------- config session -------------------------"
+useradd --uid 1000 --user-group --groups epoptes --home-dir $HOME/ --shell /bin/bash default_headless
+cp /usr/share/applications/epoptes.desktop $HOME/.config/autostart
+chmod +x $HOME/.config/autostart/epoptes.desktop
+cp $HOME/.config/autostart/epoptes.desktop $HOME/Desktop/
+echo -e "[Desktop Entry]\nHidden=True" > $HOME/.config/autostart/epoptes-client.desktop
 
 ## cascade the next start script
 echo -e "\n------------------- vnc_startup.sh -------------------------"
-/dockerstartup/vnc_startup.sh $@
+# /dockerstartup/vnc_startup.sh $@
+su default_headless --pty --preserve-environment -c "/dockerstartup/vnc_startup.sh $@"
