3abcaba13d140a2f40f1a6eaaf2101b43d49ec16
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2022-06-30 19:18:52 -0600

    aci-connector fix identity service principal

--- aks/prepare.ps1
+++ aks/prepare.ps1
@@ -11,9 +11,8 @@ Import-Module Az.Resources
 Login-AzAccount
 $ssh = (Get-Content '~/.ssh/id_rsa.pub')
 
-Set-Item env:aks_resource_group 'docker-swarm'
-Set-Item env:aks_location 'westcentralus'
-
+$resource_group = 'docker-swarm'
+$cluster_location = 'westcentralus'
 $cluster_name = 'swarm'
 $dns_name = 'cluster'
 $vnet_name = 'cluster-vnet'
@@ -25,10 +24,15 @@ $vnet_subnet_aci_address = '10.241.0.0/16'
 $cluster_service_cidr = '10.0.0.0/16'
 $cluster_dns_ip='10.0.0.10'
 $cluster_docker_cidr = '172.17.0.1/16'
+$service_principal = 'virtual-kubelet-sp'
+
+# =======================================================================================
 
 New-AzResourceGroup `
-    -Name $env:aks_resource_group `
-    -Location $env:aks_location
+    -Name $resource_group `
+    -Location $cluster_location
+
+# =======================================================================================
 
 $cluster_vnet = New-AzVirtualNetworkSubnetConfig `
     -Name $vnet_subnet_k8s_name `
@@ -39,15 +43,13 @@ $aci_vnet = New-AzVirtualNetworkSubnetConfig `
     -AddressPrefix $vnet_subnet_aci_address
 
 $vnet = New-AzVirtualNetwork `
-    -ResourceGroupName $env:aks_resource_group `
-    -Location $env:aks_location `
+    -ResourceGroupName $resource_group `
+    -Location $cluster_location `
     -Name $vnet_name `
     -AddressPrefix $vnet_address `
     -Subnet $cluster_vnet, $aci_vnet
 
-$sp = New-AzADServicePrincipal `
-    -DisplayName 'virtual-kubelet-sp'
-
+$sp = New-AzADServicePrincipal -DisplayName $service_principal
 $pw = ConvertTo-SecureString -AsPlainText $sp.PasswordCredentials.SecretText
 $cred = [System.Management.Automation.PSCredential]::new($sp.AppId, $pw)
 
@@ -56,8 +58,14 @@ New-AzRoleAssignment `
     -Scope ($vnet.Id) `
     -ApplicationId ($sp.AppId)
 
+# =======================================================================================
+
+$cluster_vnet = Get-AzVirtualnetwork | 
+    Get-AzVirtualNetworkSubnetConfig `
+        -Name $vnet_subnet_k8s_name
+
 New-AzAksCluster `
-    -ResourceGroupName $env:aks_resource_group `
+    -ResourceGroupName $resource_group `
     -Name $cluster_name `
     -SshKeyValue $ssh `
     -KubernetesVersion '1.22.6' `
@@ -74,15 +82,21 @@ New-AzAksCluster `
     -NodePoolMode System `
     -NodeVmSetType VirtualMachineScaleSets `
     -NodeVmSize 'Standard_A2_v2' `
-    -SubnetName $vnet_subnet_k8s_name `
     -NodeVnetSubnetID $cluster_vnet.Id `
+    -SubnetName $vnet_subnet_k8s_name `
     -EnableRbac:$false `
     -ServicePrincipalIdAndSecret $cred `
     -Verbose
 
 $cluster = Get-AzAksCluster `
-    -ResourceGroupName $env:aks_resource_group `
+    -ResourceGroupName $resource_group `
     -Name $cluster_name 
+
+# =======================================================================================
+
+# $cluster | Enable-AzAksAddOn `
+#     -SubnetName $vnet_subnet_aci_name `
+#     -Name VirtualNode
 
 # choco install kubernetes-helm
 # Install-AzAksKubectl -Destination (Get-Item .)
@@ -92,20 +106,24 @@ $release='virtual-kubelet'
 $vk_release='virtual-kubelet-latest'
 $chart="https://github.com/virtual-kubelet/azure-aci/raw/master/charts/$vk_release.tgz"
 
-helm uninstall virtual-kubelet
+# helm uninstall "$release" --namespace='kube-system'
 helm install "$release" "$chart" `
     --set provider=azure `
+    --set rbac.install=true `
     --set providers.azure.targetAKS=true `
+    --set providers.azure.managedIdentityID="$($sp.Id)" `
     --set providers.azure.vnet.enabled=true `
-    --set providers.azure.vnet.vnetResourceGroup="$env:aks_resource_group" `
+    --set providers.azure.vnet.vnetResourceGroup="$resource_group" `
     --set providers.azure.vnet.vnetName="$vnet_name" `
     --set providers.azure.vnet.subnetName="$vnet_subnet_aci_name" `
     --set providers.azure.vnet.subnetCidr="$vnet_subnet_aci_address" `
     --set providers.azure.vnet.clusterCidr="$vnet_subnet_k8s_address" `
     --set providers.azure.vnet.kubeDnsIp="$cluster_dns_ip" `
     --set providers.azure.masterUri="https://$($cluster.Fqdn)" `
-    --set nodeName='workerpool' `
+    --set nodeName='aci-connector' `
     --namespace='kube-system'
+
+# =======================================================================================
 
 $cluster | Import-AzAksCredential -ConfigPath (Join-Path (Get-Item .) 'kube-config') -Force
 # kubectl delete configmap server
